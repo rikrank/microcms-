93e990b6157e91c6475ed9896560a0e12e1d7fa2
rikrank <morishita-r@tam-tam.co.jp>
2023-10-04 04:56:28 +0900

    Fx：

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,18 +5,18 @@ import { SITEMAP_LIMIT } from '@/_constants';
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const data = await getArticleList({
     limit: SITEMAP_LIMIT,
-    fields: 'id,revisedAt',
+    // fields: 'id,revisedAt',
   });
   const baseUrl = process.env.BASE_URL as string;
 
   return [
     {
       url: baseUrl,
-      lastModified: data.contents[0].revisedAt,
+      // lastModified: data.contents[0].revisedAt,
     },
     ...data.contents.map((content) => ({
       url: `${baseUrl}/articles/${content.id}`,
-      lastModified: content.revisedAt,
+      // lastModified: content.revisedAt,
     })),
   ];
 }
